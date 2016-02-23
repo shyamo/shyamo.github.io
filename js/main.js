eff4932d48db975deb93e0bbd75a51ddf37cb253
sshyamo <shyamb@2JDVR32.ad.realise.com>
2016-02-23 11:42:51 +0000

    style update to labs page

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var app = {
+    init: function() {
+        app.tileHeight();
+        //app.resize();
+    },
+    
+    tileHeight: function() {
+        var maxHeight =0;
+        $('.project-title').each(function() {
+            if ( $(this).outerHeight() > maxHeight ) {
+                maxHeight = $(this).outerHeight();
+            }
+        });
+        //set all heights the same
+        $('.project-title').height(maxHeight) ;
+    },
+    
+    resize: function () {
+        $(window).resize(function() {
+            app.tileHeight();
+        });
+    }
+}
+
+$(document).ready(function() {
+    app.init();
+});
