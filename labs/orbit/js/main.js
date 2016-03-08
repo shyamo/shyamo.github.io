49c52b5ac7eac0020f1e77066f299005d6491873
sshyamo <shyamb@ad.realise.com>
2016-03-08 14:22:45 +0000

    orbit project

---
+++ labs/orbit/js/main.js
@@ -0,0 +1,29 @@
+var game = {
+  
+    variables:  {
+        
+    },
+    
+    init: function() {
+        console.log("//game initialized//");
+        game.setup();
+        game.events();
+    },
+    
+    events: function() {
+        
+    },
+    
+    setup: function() {
+        
+    },
+    
+    play: function(timeout) {
+    
+    },
+    
+};
+
+$(document).ready(function() {
+    game.init();
+});
